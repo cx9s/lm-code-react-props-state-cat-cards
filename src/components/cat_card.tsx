fd0b0ab0809e28b1886f7d08f71c224106350a84
cx9s <cx9918@gmail.com>
2023-01-31 21:00:51 +0000

    apply props into CatCard

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,10 +1,20 @@
-const CatCard: React.FC = () => (
-  <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
-  </div>
-);
+interface CatCardProps {
+  name: string;
+  species: string;
+  favFoods: string[];
+  birthYear: number;
+}
+
+const CatCard: React.FC<CatCardProps> = (props) => {
+  const favFoodsText = props.favFoods.join(", ");
+  return (
+    <div className="card">
+      <h3 className="card__text card__header">{props.name}</h3>
+      <p className="card__text">Species: {props.species}</p>
+      <p className="card__text">Favourite Food(s): {favFoodsText}</p>
+      <p className="card__text">Birth Year: {props.birthYear}</p>
+    </div>
+  );
+};
 
 export default CatCard;
