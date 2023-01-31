d881d98624bea1552d37ad047c6f5e0f45b1c34a
cx9s <cx9918@gmail.com>
2023-01-31 21:04:17 +0000

    change props into catObject

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,18 +1,17 @@
+import Cat from "../data/cat";
+
 interface CatCardProps {
-  name: string;
-  species: string;
-  favFoods: string[];
-  birthYear: number;
+  catObject: Cat;
 }
 
 const CatCard: React.FC<CatCardProps> = (props) => {
-  const favFoodsText = props.favFoods.join(", ");
+  const favFoodsText = props.catObject.favFoods.join(", ");
   return (
     <div className="card">
-      <h3 className="card__text card__header">{props.name}</h3>
-      <p className="card__text">Species: {props.species}</p>
+      <h3 className="card__text card__header">{props.catObject.name}</h3>
+      <p className="card__text">Species: {props.catObject.species}</p>
       <p className="card__text">Favourite Food(s): {favFoodsText}</p>
-      <p className="card__text">Birth Year: {props.birthYear}</p>
+      <p className="card__text">Birth Year: {props.catObject.birthYear}</p>
     </div>
   );
 };
